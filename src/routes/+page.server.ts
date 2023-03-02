71b87dfbbb7f2a66b0002b909263ab1fd4554bbb
richardcoldwell74 <richardcoldwell74@gmail.com>
2023-03-02 00:27:49 +0000

    change to .server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load = (async ({ fetch }) => {
+export const load = (async ({ fetch, setHeaders }) => {
 	const all = [];
 	for (let index = 1; index <= 10; index++) {
 		const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + index);
 		const data = await res.json();
 		all.push(data);
 	}
+	setHeaders({ 'cache-control': 'max-age=60' });
 	return { all_pokemon: all };
-}) satisfies PageLoad;
+}) satisfies PageServerLoad;
