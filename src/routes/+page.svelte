2feb884d1adca5a1c3c4edf425527f7185f02d04
richardcoldwell74 <richardcoldwell74@gmail.com>
2023-03-03 01:18:03 +0000

    change image url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,19 @@
 	export let data: PageData;
 	$: ({ allPokemon } = data);
 	$: firstTwenty = allPokemon.slice(0, 20);
+	console.log(data);
 </script>
 
 {#each firstTwenty as pokemon}
 	<h3 class="white-text">{pokemon.name}</h3>
-	<img
+	<!-- <img
 		src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' +
+			pokemon.id +
+			'.png'}
+		alt={'${allPokemon[0].name}'}
+	/> -->
+	<img
+		src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
 			pokemon.id +
 			'.png'}
 		alt={'${allPokemon[0].name}'}
