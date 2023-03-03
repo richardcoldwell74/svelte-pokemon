362e2ba3180e2f2be5a2eb1767c23061ec4efa33
richardcoldwell74 <richardcoldwell74@gmail.com>
2023-03-03 01:40:04 +0000

    Update +page.svelte

--- src/routes/types/[type]/+page.svelte
+++ src/routes/types/[type]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	export const prerender = true;
 	import type { PageData } from './$types';
 	export let data: PageData;
 	$: ({ allPokemon } = data);
